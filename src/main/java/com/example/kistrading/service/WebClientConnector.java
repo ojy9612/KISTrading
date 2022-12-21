e936c64c9d294ab5ff6853c5d336e29744fd2954
ojy9612 <pvnm@naver.com>
2022-12-21 12:25:45 +0900

    Webclient 범용성 있게 수정

--- src/main/java/com/example/kistrading/service/WebClientConnector.java
+++ src/main/java/com/example/kistrading/service/WebClientConnector.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+@Component
 @RequiredArgsConstructor
 public class WebClientConnector<T> {
     private final ObjectMapper objectMapper;
