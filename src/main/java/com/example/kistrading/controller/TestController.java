b8245392a63fae8f75c277dea29a340404ca86e1
ojy9612 <pvnm@naver.com>
2022-12-21 11:36:43 +0900

    Test Controller

--- src/main/java/com/example/kistrading/controller/TestController.java
+++ src/main/java/com/example/kistrading/controller/TestController.java
@@ -1,15 +1,23 @@
 package com.example.kistrading.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.kistrading.entity.Token;
+import com.example.kistrading.repository.TokenRepository;
+import com.example.kistrading.service.WebClientConnector;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,64 +31,110 @@ public class TestController {
     @Value("${kis.train.appsecret}")
     private String appsecert;
 
-    private final ObjectMapper objectMapper;
+
+    private final WebClientConnector webClientConnector;
+    private final TokenRepository tokenRepository;
+
+    @GetMapping("/test1")
+    @Transactional
+    public JsonNode test1() {
 
 
-    @GetMapping("/test")
-    public String test() {
-        WebClient webClient = WebClient.builder()
-                .baseUrl(train)
-                .defaultHeader("Content-Type", "application/json")
-                .build();
+        Map<String, String> reqBody = new HashMap<>();
+        MultiValueMap<String, String> reqParam = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> reqHeader = new LinkedMultiValueMap<>();
 
+        reqBody.put("grant_type", "client_credentials");
+        reqBody.put("appkey", appkey);
+        reqBody.put("appsecret", appsecert);
 
-        Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("grant_type", "client_credentials");
-        requestBody.put("appkey", appkey);
-        requestBody.put("appsecret", appsecert);
+        JsonNode jsonNode = webClientConnector.connectKIS(HttpMethod.POST, "/oauth2/tokenP",
+                reqHeader, reqParam, reqBody);
 
-        String s;
-        try {
-            s = objectMapper.writeValueAsString(requestBody);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        if (jsonNode.get("error_code") != null) {
+            return jsonNode;
         }
 
-        return webClient.post()
-                .uri("/oauth2/tokenP")
-                .bodyValue(s)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        Token token = Token.builder()
+                .token("Bearer " + jsonNode.get("access_token").asText())
+                .expiredDate(LocalDateTime.parse(
+                        jsonNode.get("access_token_token_expired").asText(),
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss")))
+                .build();
+
+        tokenRepository.save(token);
+
+        return jsonNode;
     }
+
 
     @GetMapping("/test2/{token}")
     public String test2(@PathVariable String token) {
-        WebClient webClient = WebClient.builder()
-                .baseUrl(train)
-                .defaultHeader("Content-Type", "application/json")
-                .build();
 
-        Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("token", token);
-        requestBody.put("appkey", appkey);
-        requestBody.put("appsecret", appsecert);
+        Map<String, String> reqBody = new HashMap<>();
+        MultiValueMap<String, String> reqParam = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> reqHeader = new LinkedMultiValueMap<>();
 
-        String s;
-        try {
-            s = objectMapper.writeValueAsString(requestBody);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+        reqBody.put("token", token);
+        reqBody.put("appkey", appkey);
+        reqBody.put("appsecret", appsecert);
+
+
+        JsonNode test = webClientConnector.connectKIS(HttpMethod.POST, "/oauth2/revokeP",
+                reqHeader, reqParam, reqBody);
+
+
+        return null;
+
+
+    }
+
+    @GetMapping("/test3")
+    @Transactional
+    public JsonNode test3() {
+
+
+        Map<String, String> reqBody = new HashMap<>();
+        MultiValueMap<String, String> reqParam = new LinkedMultiValueMap<>();
+        MultiValueMap<String, String> reqHeader = new LinkedMultiValueMap<>();
+
+        List<Token> all = tokenRepository.findAll();
+
+        reqHeader.add("authorization", all.get(0).getToken());
+        reqHeader.add("appkey", appkey);
+        reqHeader.add("appsecret", appsecert);
+        reqHeader.add("tr_id", "VTTC8434R");
+
+        reqParam.add("CANO", "50076882");
+        reqParam.add("ACNT_PRDT_CD", "01");
+        reqParam.add("AFHR_FLPR_YN", "N");
+        reqParam.add("OFL_YN", "");
+        reqParam.add("INQR_DVSN", "01");
+        reqParam.add("UNPR_DVSN", "01");
+        reqParam.add("FUND_STTL_ICLD_YN", "N");
+        reqParam.add("FNCG_AMT_AUTO_RDPT_YN", "N");
+        reqParam.add("PRCS_DVSN", "01");
+        reqParam.add("CTX_AREA_FK100", "");
+        reqParam.add("CTX_AREA_NK100", "");
+
+
+        JsonNode jsonNode = webClientConnector.connectKIS(HttpMethod.GET, "/uapi/domestic-stock/v1/trading/inquire-balance",
+                reqHeader, reqParam, reqBody);
+
+        if (jsonNode.get("error_code") != null) {
+            return jsonNode;
         }
 
-        return webClient.post()
-                .uri("/oauth2/revokeP")
-                .bodyValue(s)
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
+        Token token = Token.builder()
+                .token(jsonNode.get("access_token").asText())
+                .expiredDate(LocalDateTime.parse(
+                        jsonNode.get("access_token_token_expired").asText(),
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm:ss")))
+                .build();
 
+        tokenRepository.save(token);
 
+        return jsonNode;
     }
 
 
